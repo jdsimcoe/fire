1a18806072b4f6b9f452a414513646414370f24b
Jonathan Simcoe <jdsimcoe@gmail.com>
2014-09-05 09:02:35 -0700

    adding logged_in_author datasource

--- workspace/data-sources/data.logged_in_author.php
+++ workspace/data-sources/data.logged_in_author.php
@@ -1,72 +1,76 @@
 <?php
 
-	require_once(TOOLKIT . '/class.datasource.php');
+require_once TOOLKIT . '/class.datasource.php';
 
-	Class datasourcelogged_in_author extends Datasource{
+class datasourcelogged_in_author extends AuthorDatasource
+{
+    public $dsParamROOTELEMENT = 'logged-in-author';
+    public $dsParamORDER = 'desc';
+    public $dsParamREDIRECTONEMPTY = 'no';
+    public $dsParamREDIRECTONFORBIDDEN = 'no';
+    public $dsParamREDIRECTONREQUIRED = 'no';
+    public $dsParamSORT = 'id';
 
-		public $dsParamROOTELEMENT = 'logged-in-author';
-		public $dsParamORDER = 'desc';
-		public $dsParamREDIRECTONEMPTY = 'no';
-		public $dsParamSORT = 'id';
-		
+    public $dsParamFILTERS = array(
+        'username' => '{$cookie-username:0}',
+    );
 
-		public $dsParamFILTERS = array(
-				'username' => '{$cookie-username:0}',
-		);
-		
+    public $dsParamINCLUDEDELEMENTS = array(
+        'username'
+    );
 
-		public $dsParamINCLUDEDELEMENTS = array(
-				'username'
-		);
-		
+    public function __construct($env = null, $process_params = true)
+    {
+        parent::__construct($env, $process_params);
+        $this->_dependencies = array();
+    }
 
-		public function __construct(&$parent, $env=NULL, $process_params=true){
-			parent::__construct($parent, $env, $process_params);
-			$this->_dependencies = array();
-		}
+    public function about()
+    {
+        return array(
+            'name' => 'Logged in Author',
+            'author' => array(
+                'name' => 'Jonathan Simcoe',
+                'website' => 'http://fire.dev',
+                'email' => 'jonathan@simko.io'),
+            'version' => 'Symphony 2.5.0-rc.2',
+            'release-date' => '2014-09-05T16:02:11+00:00'
+        );
+    }
 
-		public function about(){
-			return array(
-				'name' => 'Logged in Author',
-				'author' => array(
-							'name' => 'Symphony Team',
-							'website' => 'http://getsymphony.com',
-							'email' => 'team@getsymphony.com'),
-				'version' => '1.0',
-				'release-date' => '2012-03-10'
-			);
-		}
+    public function getSource()
+    {
+        return 'authors';
+    }
 
-		public function getSource(){
-			return 'authors';
-		}
+    public function allowEditorToParse()
+    {
+        return true;
+    }
 
-		public function allowEditorToParse(){
-			return true;
-		}
+    public function execute(array &$param_pool = null)
+    {
+        $result = new XMLElement($this->dsParamROOTELEMENT);
 
-		public function grab(&$param_pool=NULL){
-			$result = new XMLElement($this->dsParamROOTELEMENT);
+        try{
+            $result = parent::execute($param_pool);
+        } catch (FrontendPageNotFoundException $e) {
+            // Work around. This ensures the 404 page is displayed and
+            // is not picked up by the default catch() statement below
+            FrontendPageNotFoundExceptionHandler::render($e);
+        } catch (Exception $e) {
+            $result->appendChild(new XMLElement('error', $e->getMessage() . ' on ' . $e->getLine() . ' of file ' . $e->getFile()));
+            return $result;
+        }
 
-			try{
-				
-				include(TOOLKIT . '/data-sources/datasource.author.php');
-			}
-			catch(FrontendPageNotFoundException $e){
-				// Work around. This ensures the 404 page is displayed and
-				// is not picked up by the default catch() statement below
-				FrontendPageNotFoundExceptionHandler::render($e);
-			}
-			catch(Exception $e){
-				$result->appendChild(new XMLElement('error', $e->getMessage()));
-				return $result;
-			}
+        if ($this->_force_empty_result) {
+            $result = $this->emptyXMLSet();
+        }
 
-			if($this->_force_empty_result) $result = $this->emptyXMLSet();
+        if ($this->_negate_result) {
+            $result = $this->negateXMLSet();
+        }
 
-			
-
-			return $result;
-		}
-
-	}
+        return $result;
+    }
+}
